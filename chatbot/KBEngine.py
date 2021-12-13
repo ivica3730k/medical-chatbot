245196ab3bd42d9d5f4bed88b373c197de5dcef3
Ivica <ivica-matic@outlook.com>
2021-12-13 13:06:45 +0000

    Commiting working version

--- chatbot/KBEngine.py
+++ chatbot/KBEngine.py
@@ -17,6 +17,15 @@ def load_knowledge_base(filepath: str) -> None:
 
 
 def prove(subject: str, object: str) -> str:
+    """
+
+    Args:
+        subject:
+        object:
+
+    Returns:
+
+    """
     expr = read_expr(subject + '(' + object + ')')
     answer = ResolutionProver().prove(expr, _knowledge_base, verbose=True)
     print(answer)
@@ -25,66 +34,66 @@ def prove(subject: str, object: str) -> str:
     else:
         return 'It may not be true.'
 
-
-def add_negation(subject: str, object: str) -> None:
-    """
-    Adds negation to knowledge_base, not saving to csv file
-
-    Rick is not here.
-    Args:
-        subject: Term subject
-        object: Term object
-
-    """
-    expr = read_expr(subject + '(-' + object + ')')
-    # >>> ADD SOME CODES HERE to make sure expr does not contradict
-    # with the KB before appending, otherwise show an error message.
-    _knowledge_base.append(expr)
-
-
-def add_conjunction(subject: str, object: str) -> None:
-    """
-    Adds conjunction to knowledge_base, not saving to csv file
-
-    She usually eats at home, because she likes cooking.
-    Args:
-        subject: Term subject
-        object: Term object
-
-    """
-    expr = read_expr('-' + subject + '(' + object + ')')
-    # >>> ADD SOME CODES HERE to make sure expr does not contradict
-    # with the KB before appending, otherwise show an error message.
-    _knowledge_base.append(expr)
-
-
-def add_disjunction(subject: str, object: str) -> None:
-    """
-    Adds disjunction to knowledge_base, not saving to csv file
-
-
-    Args:
-        subject: Term subject
-        object: Term object
-
-    """
-    expr = read_expr(subject + '(' + object + ')')
-    # >>> ADD SOME CODES HERE to make sure expr does not contradict
-    # with the KB before appending, otherwise show an error message.
-    _knowledge_base.append(expr)
-
-
-def add_implication(subject: str, object: str) -> None:
-    """
-    Adds implication to knowledge_base, not saving to csv file
-
-    Tim is british
-    Args:
-        subject: Term subject
-        object: Term object
-
-    """
-    expr = read_expr('->' + subject + '(' + object + ')')
-    # >>> ADD SOME CODES HERE to make sure expr does not contradict
-    # with the KB before appending, otherwise show an error message.
-    _knowledge_base.append(expr)
+# def add_negation(subject: str, object: str) -> None:
+#     """
+#     Adds negation to knowledge_base, not saving to csv file
+#
+#     Rick is not here.
+#     Args:
+#         subject: Term subject
+#         object: Term object
+#
+#     """
+#     expr = read_expr(subject + '(-' + object + ')')
+#     # >>> ADD SOME CODES HERE to make sure expr does not contradict
+#     # with the KB before appending, otherwise show an error message.
+#     _knowledge_base.append(expr)
+#
+#
+# def add_conjunction(subject: str, object: str) -> None:
+#     """
+#     Adds conjunction to knowledge_base, not saving to csv file
+#
+#     She usually eats at home, because she likes cooking.
+#     Args:
+#         subject: Term subject
+#         object: Term object
+#
+#     """
+#     expr = read_expr('-' + subject + '(' + object + ')')
+#     # >>> ADD SOME CODES HERE to make sure expr does not contradict
+#     # with the KB before appending, otherwise show an error message.
+#     _knowledge_base.append(expr)
+#
+#
+# def add_disjunction(subject: str, object: str) -> None:
+#     """
+#     Adds disjunction to knowledge_base, not saving to csv file
+#
+#
+#     Args:
+#         subject: Term subject
+#         object: Term object
+#
+#     """
+#     expr = read_expr(subject + '(' + object + ')')
+#     # >>> ADD SOME CODES HERE to make sure expr does not contradict
+#     # with the KB before appending, otherwise show an error message.
+#     _knowledge_base.append(expr)
+#
+#
+# def add_implication(subject: str, object: str) -> None:
+#     """
+#     Adds implication to knowledge_base, not saving to csv file
+#
+#     Tim is british
+#     Args:
+#         subject: Term subject
+#         object: Term object
+#
+#     """
+#     expr = read_expr('->' + subject + '(' + object + ')')
+#     # >>> ADD SOME CODES HERE to make sure expr does not contradict
+#     # with the KB before appending, otherwise show an error message.
+#     _knowledge_base.append(expr)
+#
