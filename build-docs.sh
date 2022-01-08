a1949c8f894f3b4089861b236909698766573400
Ivica Matic <ivica-matic@outlook.com>
2022-01-08 11:40:29 +0000

    Doc script update

--- build-docs.sh
+++ build-docs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+rm -rf ./docs
+mkdir -p docs/html
 pdoc3 --force --html -o docs/html chatbot/
 mkdir -p docs/pdf
 pdoc3 --force --pdf -o docs/pdf chatbot/ | pandoc  -o docs/pdf/chatbot.pdf
